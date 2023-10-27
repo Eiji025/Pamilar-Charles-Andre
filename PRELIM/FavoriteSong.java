27a5e4b88a6349946ca2f0d2e522c16f9e1b60c0
Charles Andre Pamilar <116739197+Eiji025@users.noreply.github.com>
2023-10-27 19:00:37 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,15 @@
+public class FavoriteSong  {
+   public static void main(String[] args) {
+      String FirstSong  = "ERE | BY: Juan Carlos";
+      String SecondSong = "NAHUHULOG | BY: Jed Baruelo";
+      String ThirdSong  = "A THOUSAND YEARS | By: James Arthur";
+      String FourthSong = "PANININDIGAN KITA | By: Ben&Ben";
+      
+      System.out.println("THESE ARE MY FAVORITE SONGS:");   
+      System.out.println("1.) " +FirstSong);
+      System.out.println("2.) " +SecondSong);
+      System.out.println("3.) " +ThirdSong);
+      System.out.println("4.) " +FourthSong);
+      
+  }
+ }
